4cfdb1ada485085421509a686634e63e4574239c
MaxiSoriaGit <maximilianosoria42@gmail.com>
2026-05-27 12:16:59 -0300

    se agrego la actividad 1

---
+++ datos-personales.js
@@ -0,0 +1,8 @@
+let nombreCompleto = 'Maximiliano Soria';
+let edad = '26';
+const carrera = 'Tecnicatura Superior en Desarrollo de Software';
+const cursando  = 'true';
+ 
+
+console.log(`console.log('mi nombre es ${nombreCompleto},tengo ${edad}, estudio la carrera de ${carrera} 
+y la estoy cursando ${cursando}`);  
